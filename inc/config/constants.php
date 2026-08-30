001caf101f47fbb8dcd55924847638908659b7fc
nneliifeanyi <stanvicbest@gmail.com>
2026-08-30 11:53:44 +0100

    live leastar DB changes

--- inc/config/constants.php
+++ inc/config/constants.php
@@ -1,21 +1,27 @@
 <?php
-// Root url for the site
-define('ROOT_URL', 'http://localhost/inventory_system/');
 
+// Detect HTTPS for production and local development alike.
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || (!empty($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443)
+    ? 'https://'
+    : 'http://';
 
-// Database parameters
-// Data source name
-define('DSN', 'mysql:host=localhost;port=3306;dbname=leadstar_inventory');
+define('PROTOCOL', $protocol);
 
-// Hostname
-define('DB_HOST', 'localhost');
+if (isset($_SERVER['HTTP_HOST']) && (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false || strpos($_SERVER['HTTP_HOST'], '127.0.0.1') !== false)) {
+    // Local development environment
+    define('ROOT_URL', 'http://localhost/inventory_system/');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'leadstar_inventory');
+} else {
+    // Online production environment
+    define('ROOT_URL', PROTOCOL . 'leadstar.com.ng/inventory/');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'leadstar_blessed_oz');
+    define('DB_PASSWORD', 'Avalanche@25');
+    define('DB_NAME', 'leadstar_blessed_oz');
+}
 
-// DB user
-define('DB_USER', 'root');
-
-// DB password
-define('DB_PASSWORD', '');
-
-// DB name
-define('DB_NAME', 'leadstar_inventory');
+define('DSN', 'mysql:host=' . DB_HOST . ';port=3306;dbname=' . DB_NAME);
 
